bf0113e3fe964c2812a5b984ae79d02135317a76
Nicolas Bello Camilletti <nbellocamilletti@gmail.com>
2017-10-02 23:30:30 -0300

    Update expense to use inputs and update total dynamically

--- src/public/js/expenses.js
+++ src/public/js/expenses.js
@@ -1,7 +1,17 @@
 function createExpenseDetailElement(detail) {
     const element = document.createElement('li');
     element.className = 'list-group-item expense';
-    element.innerHTML = `${detail.name}<span class="total">${detail.cost}</span>`;
+    element.innerHTML = 
+    `<form>
+        <div class="form-row">
+            <div class="col">
+                <input type="text" class="form-control name" value="${detail.name}">
+            </div>
+            <div class="col">
+                <input type="text" class="form-control cost" value="${detail.cost}">
+            </div>
+        </div>
+    </form>`;
 
     return element;
 }
@@ -10,6 +20,26 @@ function getExpenseId() {
     const href = location.href;
     const id = href.substr(href.lastIndexOf('/') + 1)
     return parseInt(id, 10);
+}
+
+function updateValues() {
+    const expenseId = getExpenseId();
+    const expense = getExpense(expenseId);
+    const expensesListElement = document.querySelector('#expenses-list');
+    const expenseElements = expensesListElement.querySelectorAll('.expense');
+    let details = [];
+
+    expenseElements.forEach(e => {
+        details.push({
+            name: e.querySelector('.form-control.name').value,
+            cost: parseInt(e.querySelector('.form-control.cost').value,10)
+        });
+    });
+
+    expense.details = details;
+
+    const totalField = expensesListElement.querySelector('.total-row .total');
+    totalField.innerHTML = getExpenseTotal(expense);
 }
 
 function updateExpensesView() {
@@ -22,8 +52,13 @@ function updateExpensesView() {
     });
 
     const expense = getExpense(expenseId);
+
+    const expenseName = document.querySelector('#expense-name');
+    expenseName.innerHTML = expense.name;
     expense.details.forEach(detail => {
-        expensesListElement.appendChild(createExpenseDetailElement(detail));
+        const e = createExpenseDetailElement(detail);
+        e.addEventListener('input', () => { updateValues(); });
+        expensesListElement.appendChild(e);
     });
 
     const totalField = expensesListElement.querySelector('.total-row .total');
